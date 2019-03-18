9c33a90be04add2a36364c84940f13938718f84b
Your Name <you@example.com>
2019-03-18 08:36:09 -0400

    need to test but can't yet

---
+++ src/clientserverpackage/Client.java
@@ -0,0 +1,51 @@
+package clientserverpackage;
+import java.io.*;
+import java.net.*;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Client {
+	
+	static PriorityQueue<String> packetQ = new PriorityQueue<String>();
+    public static void main(String[] args) throws IOException {
+        
+    	Scanner keyboard = new Scanner(System.in);
+    	System.out.println("Enter the host name");
+        String hostName = keyboard.nextLine();//192.168.1.129
+    	System.out.println("Enter the port number");
+        int portNumber = Integer.parseInt(keyboard.nextLine());
+        keyboard.close();
+
+        try (
+            Socket echoSocket = new Socket(hostName, portNumber);
+        	PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+            ) 
+        {
+        	System.out.println("Connected to socket - host="+hostName+" and port="+portNumber);
+        	
+        	while(true)
+			{
+        		String serverMessage = in.readLine();
+        		if(serverMessage.equals("YOU_HAVE_RECEIVED_ALL_PACKETS"))
+        			break;
+        		//echo first two characters which is id
+				out.println(serverMessage.substring(0, 2));
+				// first two string digits automatically will prioritize rest of string based on ASCII?
+				packetQ.add(serverMessage);
+			}
+        	//read all packets after all have been received, leaving out ids
+        	while(!packetQ.isEmpty()) 
+        	{
+        		System.out.print(packetQ.poll().substring(2)+" ");
+        	}
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " +
+                hostName);
+            System.exit(1);
+        } 
+    }
+}
---
+++ src/clientserverpackage/Server.java
@@ -0,0 +1,85 @@
+package clientserverpackage;
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.Set;
+import java.net.*;
+import java.io.*;
+
+public abstract class Server {
+	
+	 ArrayList<String> packets;
+	public  void setPackets(String[] arr) {
+		packets = new ArrayList<>(Arrays.asList(arr));
+	}
+	
+	boolean packetProbability;
+	public abstract void setProbability();
+	
+	public void performTCPServerProtocol() {
+    	
+    	Scanner keyboard = new Scanner(System.in);
+    	System.out.println("Enter port number");
+        int portNumber = Integer.parseInt(keyboard.nextLine());
+        keyboard.close();
+        
+        try (
+            ServerSocket serverSocket =
+                new ServerSocket(portNumber);
+            Socket clientSocket = serverSocket.accept();   
+        	
+            PrintWriter out =
+                new PrintWriter(clientSocket.getOutputStream(), true);                   
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream()));
+        		
+        ) {
+        	System.out.println("Server started on port "+ portNumber);
+        	
+        	//check that there are still non-received packets
+        	while (!packets.stream().allMatch(x -> x == null))
+            {   
+        		String inputLine = in.readLine();
+        		//can parse 01 into 1?
+            	int receivedPacketNumber = Integer.parseInt(inputLine);//contains first 2 digits of packet id echoed by client
+            	//set received packets to null.
+            	packets.set(receivedPacketNumber, null);
+            	//go through all packets as long as there are non null ones - maybe needs optimization
+	            for (int i = 0; i < packets.size(); i++) 
+	            {   
+	            	String packetText = packets.get(i);
+	            	if(packetText != null) 
+	            	{
+	            		//send packets. use index in arrayList as "numerical" String id. Append "0" to id if single digit
+	            		String packetId = i >= 10 ? ""+i : "0"+i;
+	            		String fullPacket = packetId+packetText;
+	            		if(packetProbability) 
+		                {
+		                	out.println(fullPacket);
+		                }
+	            	}
+	            }	
+            }
+        	//inform client that all packets were received by client
+        	out.println("YOU_HAVE_RECEIVED_ALL_PACKETS");
+            
+        } catch (IOException e) {
+            System.out.println("Exception caught when trying to listen on port "
+                + portNumber + " or listening for a connection");
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
+
+
+
+
+
+
+
